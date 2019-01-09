b46e37e6bb5826abbff2849c1013c35f8d4bf34b
Jon Parkin <jon@jparkin.co.uk>
2019-01-09 18:10:58 +0000

    Added main panels

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import RulesDisplay from './RulesDisplay';
+import VisitorData from './VisitorData';
+import WebsiteOutput from './WebsiteOutput';
+import Description from './Description';
 
 class App extends Component {
   render() {
+
+    const ads = [
+      "No Ad #1",
+      "No Ad #2",
+      "No Ad #3",
+      "No Ad #4",
+      "No Ad #5",
+      "No Ad #6",
+      "No Ad #7",
+      "No Ad #8"
+    ];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <RulesDisplay />
+            </div>
+            <div class="col">
+              <VisitorData />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <WebsiteOutput ads={ads} />
+            </div>
+            <div class="col">
+              <Description />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
